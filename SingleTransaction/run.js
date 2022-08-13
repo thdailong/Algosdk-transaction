e07cede0d3f4860737fc0fabf7c34c0a6df52c44
MAC <mac@MacBook-Pro-cua-MAC.local>
2022-08-13 08:26:13 -0700

    News

---
+++ SingleTransaction/run.js
@@ -0,0 +1,81 @@
+import { AccountI, AccountII , AccountIII} from "../Account.js";
+import algosdk from "algosdk";
+import * as fs from "fs";
+
+const keypress = async () => {
+    process.stdin.setRawMode(true)
+    return new Promise(resolve => process.stdin.once('data', () => {
+        process.stdin.setRawMode(false)
+        resolve()
+    })) 
+}
+// Create an account and add funds to it. Copy the address off
+// The Algorand TestNet Dispenser is located here: 
+// https://dispenser.testnet.aws.algodev.network/
+
+async function firstTransaction() {
+
+    try {
+        let myAccount = AccountI;
+        // Connect your client
+        const algodToken = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+        const algodServer = 'http://localhost';
+        const algodPort = 4001;
+        let algodClient = new algosdk.Algodv2(algodToken, algodServer, algodPort);
+
+        //Check your balance
+        let accountInfo = await algodClient.accountInformation(myAccount.addr).do();
+        console.log("Account balance: %d microAlgos", accountInfo.amount);
+
+        // Construct the transaction
+        let params = await algodClient.getTransactionParams().do();
+        // comment out the next two lines to use suggested fee
+        params.fee = algosdk.ALGORAND_MIN_TX_FEE;
+        params.flatFee = true;
+
+        // receiver defined as TestNet faucet address 
+        const receiver = "HZ57J3K46JIJXILONBBZOHX6BKPXEM2VVXNRFSUED6DKFD5ZD24PMJ3MVA";
+        const enc = new TextEncoder();
+        const note = enc.encode("Hello World");
+        let amount = 100000;
+        let sender = myAccount.addr;
+        let txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+            from: sender, 
+            to: receiver, 
+            amount: amount, 
+            note: note, 
+            suggestedParams: params
+        });
+
+        let bytesUnsignedTxn = algosdk.encodeObj(txn.get_obj_for_encoding());
+        fs.writeFileSync("pay.txn", Buffer.from(bytesUnsignedTxn).toString("base64"));
+
+        // Sign the transaction
+        let signedTxn = txn.signTxn(myAccount.sk);
+        fs.writeFileSync("signed_pay.txn", Buffer.from(signedTxn).toString("base64"));
+
+        let txId = txn.txID().toString();
+        console.log("Signed transaction with txID: %s", txId);
+
+        // Submit the transaction
+        await algodClient.sendRawTransaction(signedTxn).do();
+
+        // Wait for confirmation
+        let confirmedTxn = await algosdk.waitForConfirmation(algodClient, txId, 4);
+        //Get the completed Transaction
+        console.log("Transaction " + txId + " confirmed in round " + confirmedTxn["confirmed-round"]);
+        let string = new TextDecoder().decode(confirmedTxn.txn.txn.note);
+        console.log("Note field: ", string);
+        accountInfo = await algodClient.accountInformation(myAccount.addr).do();
+        console.log("Transaction Amount: %d microAlgos", confirmedTxn.txn.txn.amt);        
+        console.log("Transaction Fee: %d microAlgos", confirmedTxn.txn.txn.fee);
+
+        console.log("Account balance: %d microAlgos", accountInfo.amount);
+    }
+    catch (err) {
+        console.log("err", err);
+    }
+    process.exit();
+};
+
+firstTransaction();
